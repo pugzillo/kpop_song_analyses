31b13854f5cac82776b25f08bd6720dd2182a4fd
Linh <lcahu91@gmail.com>
2019-09-08 15:12:29 -0700

    added loop so get_artist_tracks can be run on multiple artists

--- spotipy_test.py
+++ spotipy_test.py
@@ -5,11 +5,22 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-name = 'Twice'
-
-results = sp.search(q= name)
+name = "Seven O'Clock"
+results = sp.search(q='artist:%s genre:k-pop' % name)
+print(results)
 for i, t in enumerate(results['tracks']['items']):
     print(' ', i, t['name'])
     
-features = sp.audio_analysis('5qSoW3ewNlhRN3FNZPmVns')
-print(features)
+# features = sp.audio_analysis('5qSoW3ewNlhRN3FNZPmVns')
+# print(features)
+
+
+# # open the file and read the contents
+# f = open("List_of_KpopGroups_Wiki_Sept7_2019.csv")
+# lines = f.readlines()[1:]
+# results = []
+# for x in lines:
+#     results.append(x.split(',')[0])
+
+# f.close()
+# print(results)
